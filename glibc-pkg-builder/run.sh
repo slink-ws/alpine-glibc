1b76c9208cb002ce3108f41ec0e9ed0d20e880da
Mikhail <kami@slink.ws>
2024-01-31 11:13:44 +1000

    x86_64 build config update

--- glibc-pkg-builder/run.sh
+++ glibc-pkg-builder/run.sh
@@ -3,8 +3,8 @@
 IMAGE_TAG=mvkvl/alpine-pkg-builder
 
 mkdir -p pkg
-docker run --rm -it --entrypoint "sh"                         \
-  -v "./pkg:/home/slink/packages"                             \
-  -v "./assets/$(uname -m):/src/assets"                       \
-  $IMAGE_TAG                                                  \
+docker run --rm -it --entrypoint "sh" \
+  -v "./pkg:/home/slink/packages" \
+  -v "./assets/$(uname -m):/src/assets" \
+  $IMAGE_TAG \
   -c "cp /src/assets/* /src && abuild checksum && abuild -r"
